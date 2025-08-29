ae30f2b6b0dbf9ace85895b555be5f5f9d1e24b5
Filipe-F0105 <rodrigues.filipe511@gmail.com>
2025-08-29 21:37:31 +0000

    criei pritf cirei scanf

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -2,15 +2,30 @@
 
 
 int main() {
-printf("Desafio de Cartas Super Trunfo");    
+//printf("Desafio de Cartas Super Trunfo");    
 
-int População, Numero de Pontos Turisticos;
-float Área, Pib;
-
-
-
-
+int População;
+int ("Pontos Turisticos");
+float Área; 
+float Pib;
 
 
-    return 0;
+printf("Pontos Turisticos:\n");
+scanf("%d", Ponto Turisticos);
+
+printf("População:\n");
+scanf("%d"  População);
+
+printf("Área:\n");
+scanf("%f"  Área);
+
+printf("PIB\n");
+scanf("%f"  Pib);
+
+printf("Total da Populção é: %d\n", População);
+printf("Numero De Pontos Turisticos: %d\n", Pontos Turisticos);
+printf("A Área é: %f\n", Área);
+printf("O PIb é:%f\n",Pib);
+
+return 0;
 }
