6dc30d7e6a2c716aff430c48ec0852e545f63210
Filipe-F0105 <rodrigues.filipe511@gmail.com>
2025-09-02 16:40:37 -0300

    Update CartasSuperTrunfo.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -10,8 +10,6 @@ int Populacao1;
 int PontosTuristicos1;
 float Area1;
 float Pib1;
-float Densidadepopulacional1;
-float Pibpercapita1;
 
 //("Variaveis Para a Segunda Carta");
 char codigo2[4];
@@ -19,8 +17,6 @@ int Populacao2;
 int PontosTuristicos2;
 float Area2;
 float Pib2;
-float Densidadepopulacional2;
-float Pibpercapita2;
 
 // Cadastro Da Carta 1;
 printf (" Cadastro Da carta 1 \n");
@@ -39,10 +35,6 @@ scanf ("%f", &Pib1);
 printf ("Número De Pontos Turisticos: ");
 scanf ("%d",&PontosTuristicos1);
 
-// Cadastro Da Carta 1;
-Densidadepopulacional1 = Populacao1 / Area1;
-Pibpercapita1 = (Pib1 * 1000000000 )/ Populacao1;
-
 //Cadastro Da Carta 2;
 printf ("\n Cadastro Da Carta 2 \n");
 printf ("Codigo Da Carta (ex: B02): ");
@@ -60,17 +52,12 @@ scanf ("%f", &Pib2);
 printf ("Número De Pontos Turisticos: ");
 scanf ("%d", &PontosTuristicos2);
 
-//Cadastro Da Carta 2;
-Densidadepopulacional2 = Populacao2 / Area2;
-Pibpercapita2 = (Pib2 * 1000000000)/ Populacao2;
 
 printf("\nCarta 1 - %s\n", codigo1);
 printf("População: %d\n", Populacao1);
 printf("Área: %.2f\n", Area1);
 printf("PIB: %.2f bilhões\n", Pib1);
 printf("Numero De Pontos Turistico: %d\n", PontosTuristicos1);
-printf("Densidade Populacional: %.2f hab/km²\n", Densidadepopulacional1);
-printf("PIB Per Capita: %.2f reais\n", Pibpercapita1);
 
   
 printf("\nCarta 2 - %s\n", codigo2);
@@ -78,8 +65,6 @@ printf("População: %d\n", Populacao2);
 printf("Área: %.2f\n", Area2);
 printf("PIB: %.2f bilhões\n", Pib2);
 printf("Numero De Pontos Turisticos: %d\n", PontosTuristicos2);
-printf("Densidade Populacional: %.2f hab/km²\n", Densidadepopulacional2);
-printf("PIB Per Capita: %.2f reais\n", Pibpercapita2);  
+
 return 0;
-}
 }
